a85d69151ff37ab64bb049dd2a9b7852d9d75196
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-15 14:25:01 -0700

    C - Hello, World

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main- print a message
- *   * Return: 0
- *    */
+ * main- print a message
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
