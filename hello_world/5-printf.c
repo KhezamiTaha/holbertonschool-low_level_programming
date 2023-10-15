d919c81838dc9e7cf6e527db5e3afe6fc154fc2b
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-15 14:28:30 -0700

    C - Hello, World

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * * main- print a message
- * * Return: 0
- *  */
+ * main- print a message
+ * Return: 0
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
