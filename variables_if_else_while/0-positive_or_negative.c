4da86beb682bc1895b3a0495188f81962e380a78
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-17 05:16:25 -0700

    C - Hello, World

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,16 @@
 #include <stdio.h>
 #include <time.h>
 /* more headers goes there */
+ 
+/**
+   * main- Short description, single line
+    * @parameterx: Description of parameter x
+    (* a blank line
+     * Description: Longer description of the function)?
+     (* section header: Section description)*
+      * Return: Description of the returned value
+*/
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 /**
