cf4d85e54cefc7c4910a38611e76e1d87af042fd
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-19 09:27:58 -0700

    C - variables_if_else_while

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - main desc
+*
+*Return: des return
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	 putchar('\n');
+	return (0);
+}
