a209daf04628ca349a33c67429943251d2c65042
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-19 09:17:21 -0700

    C - variables_if_else_while

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - des
+ *
+ *Return: des
+ */
+int main(void)
+{
+	int nb;
+
+	for (nb = '0'; nb <= '9'; nb++)
+	{
+		putchar(nb);
+	}
+	putchar('\n');
+	return (0);
+}
