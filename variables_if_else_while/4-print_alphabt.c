a7e70a167b0dd2ae8c4398af67047f7607429b0a
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-19 00:59:13 -0700

    C - Hello, World

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - des goes here
+*
+*Return: des goes here
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
