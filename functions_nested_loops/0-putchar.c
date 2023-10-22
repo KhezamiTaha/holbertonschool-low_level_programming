cddff6ed15749080530af2596b2f446446bbdc68
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-22 09:51:42 +0100

    functiions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- *main - des
- *
- *Return: des
+ * main - Print _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar(10);
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
