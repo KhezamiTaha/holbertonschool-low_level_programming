6444a70a52185a1f7eab632addf80aebb8d2e08a
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-26 17:22:23 +0100

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_mul - Algorithms function
+ *@: integer
+ *
+ *Return: 1 or 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/*   To-Do :  Variables Description*/
