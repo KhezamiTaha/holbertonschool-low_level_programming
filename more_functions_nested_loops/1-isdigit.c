be7d89990776be9c87ee086f06634e893341725e
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-26 18:07:03 +0100

    3-print_numbers

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -14,3 +14,17 @@ int _isdigit(int c)
 	else
 		return (0);
 }
+
+
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers();
+    return (0);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_numbers - Algorithms function
+ *@i: integer
+ *
+ *Return: 1 or 0
+ */
+void print_numbers(void)
+{
+	int i= 48;
+
+	for (i= 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
+
+
+
+/*   To-Do :  Variables Description*/
