24dfd04b7d7498153d172671b9cc8dd2bb4bf903
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-27 08:11:10 +0100

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_print_most_numbers - Algorithms function
+ *@: integer
+ *
+ *Return: 1 or 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar(10);
+}
+
+
+
+
+
+/*   To-Do :  Variables Description*/
