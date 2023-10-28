bf95a8a62769a9e69ff5ff978e577aaf12cf8324
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-28 08:51:24 +0100

    6-print_line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- *
-print_line
- - Algorithms function
+ *print_line - Algorithms function
  *@: integer
  *
- *Return: 1 or 0
+] *Return: 1 or 0
  */
 void print_line(int c)
 {
+
+	int i;
+
 	if (c <= 0)
 		_putchar(10);
 	else
-	{	
-	for (i = 1; i <= c; i++)
-	{	if()}
+	{
+		for (i = 1; i <= c; i++)
+		{
+			_putchar('_');
+		}
+	}
+	if (c > 0)
+		_putchar(10);
 }
-
 
 /*   To-Do :  Variables Description*/
