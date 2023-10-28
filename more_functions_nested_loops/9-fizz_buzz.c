106595933f5e2296da33ce5525267368ccf80df4
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-28 10:55:23 +0100

    9-fizz_buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *fizz_buzz - Algorithms function
+ *@: integer
+ *
+ *Return: 1 or 0
+ */
+void fizz_buzz(void)
+{
+	int counter;
+	
+	for (counter = 1; counter <= 100; counter++)
+    {
+		if (counter % 15 == 0)
+			printf("FizzBuzz ");
+		else if (counter % 5 == 0)
+			printf("Buzz ");
+		else if (counter % 3 == 0)
+		 	printf("Fizz ");
+		else
+		printf("%d ", counter);
+	}
+	printf("\n");
+
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */ 
