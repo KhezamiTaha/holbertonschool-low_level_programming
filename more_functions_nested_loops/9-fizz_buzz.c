36d57ab932647080f60215696106d661c3595ad1
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-28 11:27:11 +0100

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,12 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (counter % 5 == 0)
 		{
-			if (counter == 100)
+			if (counter != 100)
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
-			printf("Buzz ");
+			else
+				printf("Buzz");
 		}
 
 		else if (counter % 3 == 0)
