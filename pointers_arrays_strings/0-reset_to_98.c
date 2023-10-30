a806c3e4a7e3b2f8ff7af298df76515f2abcd815
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-30 08:49:02 +0000

    0-98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - Algorithms function
+ *@n: integer pointer
+ *
+ *Return: 1 or 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
