9977a2117131b2c7103794ebb06d6b0e54773167
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-30 08:57:24 +0000

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *swap_int - Algorithms function
+ *@a: integer pointer
+ *@b: integer pointer
+ *
+ *Return: 1 or 0
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
