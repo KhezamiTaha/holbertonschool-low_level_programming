81a775135ac6dc61c2100f585882d918806484bc
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-30 12:38:32 +0000

    6-puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *puts2 - Algorithms function
+ *@str: integer
+ *
+ *Return: 1 or 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+		i++;
+	}
+	_putchar(10);
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
