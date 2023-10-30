0cc3a950b79c48a1081fd909b4311b1276c8a293
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-30 17:16:42 +0000

    9-strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strcpy - Algorithms function
+ *@dest: destination string
+ *@src: source string
+ *
+ *Return: 1 or 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	return (dest);
+}
+
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
