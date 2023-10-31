48214e5a15a5e94a504dbc7de0aecf80553d0715
KhezamiTaha <khezamitaha10@gmail.com>
2023-10-31 14:15:16 +0000

    100-atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *_atoi - Algorithms function
  *@s: array pointer
@@ -8,20 +7,16 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int len = 0;
-	int nb = 0;
-	int first = 0;
+	int i = 0, len = 0, nb = 0;
+	int plus = 0, minus = 0;
 
 	while (1)
 	{
+
 		if (isdigit(*(s + i)))
 		{
 
-			if (first == 0)
-				first = i;
-
-			if (*(s + first - 1) == '-')
+			if (minus > plus)
 			{
 
 				len = len + 1;
@@ -35,14 +30,13 @@ int _atoi(char *s)
 				nb = ((nb * 10) + (*(s + i) - 48));
 			}
 		}
+		else if (*(s + i) == '-')
+			minus = minus + 1;
+		else if (*(s + i) == '+')
+			plus = plus + 1;
 		else if ((len != 0) && !(isdigit(*(s + i))))
 			break;
 		i++;
 	}
 	return (nb);
 }
-
-/**
- * To-Do :  Variables Description
- *          Formt document
- */
