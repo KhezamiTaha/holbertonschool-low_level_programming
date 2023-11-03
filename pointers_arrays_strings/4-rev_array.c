c38254d01281f49ba1153d2173bee56e3db08ab2
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-03 12:05:36 +0000

    4-rev_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *reverse_array - Algorithms function
+ *@a: pointer
+ *@n: integer
+ *
+ *Return: 1 or 0
+ */
+void reverse_array(int *a, int n)
+{
+	int i, nb;
+
+	for (i = 0; i <= n / 2 - 1; i++)
+	{
+		nb = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = nb;
+	}
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
