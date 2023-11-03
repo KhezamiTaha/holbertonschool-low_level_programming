f7bf3cf0ad9b188faa120907435f74d454935d9d
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-03 11:47:53 +0000

    3-strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,12 +15,12 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] > s2[i])
 		{
-			return (1);
+			return (13);
 		}
 		else if (s1[i] < s2[i])
 		{
 
-			return (-1);
+			return (-13);
 		}
 
 		i++;
