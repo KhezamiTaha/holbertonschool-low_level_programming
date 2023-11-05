c0700d246afe15aeb8e7e049abda141d6c0e323e
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-05 12:48:55 +0000

    2-strchr.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -31,3 +31,4 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
