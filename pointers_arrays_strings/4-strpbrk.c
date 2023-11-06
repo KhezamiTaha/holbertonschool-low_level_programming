5f460ee8eb13b97a4e35ed16b2c71205e58a7ff6
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-06 10:09:54 +0000

    4-strpbrk.c

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
- *_strspn - Algorithms function
+ *_strpbrk - Algorithms function
  *@s: pointer
  *@accept: pointer
  *
  *Return: 1 or 0
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j, leng;
+	unsigned int i, j;
 
-	leng = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				leng = leng + 1;
+				return (s + i);
 		}
-		if (leng == i)
-			return (leng);
 	}
-	return (0);
+	return ('\0');
 }
+
 
 /**
  * To-Do :  Variables Description
  *          Formt document
- */
+ */ 
