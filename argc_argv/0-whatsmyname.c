d4f35300c7975214c8fda197939eec6adc4986d6
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-08 17:11:40 +0000

    argv, argc

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - writes the character c to stdout
+ * @argc: The character to print
+ *@argv: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char* argv[])
+{	
+	(void) argc;
+	_puts(argv[0]);
+	return (0);
+}
