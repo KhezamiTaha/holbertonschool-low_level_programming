2ee377a044ce63cc684f735dbde29942f98c98bd
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-09 09:48:56 +0000

    argv, argc

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,10 +9,10 @@
 int main(int argc, char *argv[])
 {
 	if (argc < 3)
-	{	printf("ُError\n");
+	{	printf("ُErro\n");
 		return (1);
 	}
-	else 
+	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
