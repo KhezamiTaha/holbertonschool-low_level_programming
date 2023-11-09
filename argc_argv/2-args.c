74658d27fae0ecb8a46365658fd277cea8b42bfa
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-09 09:36:28 +0000

    argv, argc

---
+++ argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - writes the character c to stdout
+ * @argc: The character to print
+ *@argv: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
