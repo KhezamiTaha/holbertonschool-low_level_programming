33fcd1c32658122092d9c4934e4363e2b18ee420
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-09 09:46:19 +0000

    argv, argc

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - writes the character c to stdout
+ * @argc: The character to print
+ *@argv: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{	printf("ُError\n");
+		return (1);
+	}
+	else 
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+
