3e1fe57612eedce24aaeb6aaa4500c22fc55526e
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-11 14:23:26 +0000

    2-str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,11 +11,10 @@ char *str_concat(char *s1, char *s2)
 {
 	char *pointer_to_new_string;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
-		return (s1);
-	}
+	if (s2 == NULL)
+		s2 = "";
 	pointer_to_new_string = malloc((strlen(s1) + strlen(s2) + 1) * sizeof(char));
 	if (pointer_to_new_string == NULL)
 	{
