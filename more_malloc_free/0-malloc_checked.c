7bd3f2e9ffe47e8fa73e8505d75216912af7721f
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-13 08:38:04 +0000

    0-malloc_checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *malloc_checked - Algorithms function
+ *@b: pointer
+ *@: pointer
+ *
+ *Return: 1 or 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		fprintf(stderr, "Memory allocation failed\n");
+		exit(98);
+	}
+	return (ptr);
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
