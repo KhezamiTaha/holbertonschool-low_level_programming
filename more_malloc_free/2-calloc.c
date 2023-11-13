a5dd5e73b41ef77d87ba9989d7e21fd6c211f47d
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-13 12:01:35 +0000

    3-array_range.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		printf("Not enough memory!");
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
 	return (ptr);
 }
