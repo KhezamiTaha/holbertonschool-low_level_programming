176dad6af143335f8524fc98788ec36a2eaf8a25
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-14 12:22:24 +0000

    3-function_like_macro

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ABS_TRICK
+#define ABS_TRICK
+#define ABS(x) (((x) < 0) ? -x : x)
+#endif
--- preprocessor/main.h
+++ preprocessor/main.h
@@ -113,3 +113,4 @@ int *array_range(int min, int max);
 #endif
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
