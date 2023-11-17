b3f493cd186146f370f28094b0d79a84fadc1271
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-17 09:18:55 +0000

    1-init_dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  *init_dog - Algorithms function
@@ -11,6 +13,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		printf("Ok\n");
+		exit(0);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
