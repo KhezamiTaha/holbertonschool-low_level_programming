82efb4bd52f70444ee751e072850666b0a3a8fef
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-17 10:01:53 +0000

    2-print_dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	else
 		printf("Name: %s\n", d->name);
 	if (d->age == 0)
-		printf("nil\n");
+		printf("Age: %f\n", d->age);
 	else
 		printf("Age: %f\n", d->age);
 	if (d->owner == NULL)
