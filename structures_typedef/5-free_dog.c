d74fba77d8abc648c5936581067b18f394d73610
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-18 21:35:48 +0000

    5-free_dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ *free_dog - Write a function that frees dogs.
+ *@d: pointer
+ *@: pointer
+ *
+ *Return: 1 or 0
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
