fe02f418186460d1eaefb63451921321c933b8bf
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-20 09:03:03 +0000

    1-array_iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - Write a function that executes a function given as a
+ *parameter on each element of an array.
+ *@array: pointer
+ *@size: pointer
+ *@action: pointer
+ *Return: 1 or 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -15,4 +15,15 @@
  *Return: 1 or 0
  */
 void print_name(char *name, void (*f)(char *));
+
+
+/**
+ *array_iterator - Write a function that executes a function given as a
+ *parameter on each element of an array.
+ *@array: pointer
+ *@size: pointer
+ *@action: pointer
+ *Return: 1 or 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
