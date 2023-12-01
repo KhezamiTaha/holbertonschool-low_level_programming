08e12da3fe1c3b7a5485257a04f9605171a1111b
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-01 16:24:17 +0000

    5-get_dnodeint

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,6 +14,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	for (i = 0; i < index; i++)
 	{
+		if (ptr == NULL)
+		{
+			printf("(nil)");
+			return (NULL);
+		}
+
 		ptr = ptr->next;
 	}
 	return (ptr);
