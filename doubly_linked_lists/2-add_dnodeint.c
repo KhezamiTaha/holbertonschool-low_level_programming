802c72f966310270d6d2ded47c695dca82f8e5ef
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-01 10:25:26 +0000

    2-add_dnodeint

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - Algorithms function
+ *@head: pointer to pointer to struct
+ *@n: pointer
+ *
+ *Return: 1 or 0
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
+
+	if (temp == NULL)
+	{
+		printf("Error\n");
+		return (NULL);
+	}
+
+	temp->n = n;
+	temp->next = *head;
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
