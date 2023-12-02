af8070e0f686721ce34b33fde8c29a8a02505b13
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-02 09:32:35 +0000

    7-insert_dnodeint

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,32 +16,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp == NULL)
 	{
-		printf("Error\n");
 		return (NULL);
 	}
 
-	temp->n = n;
-
-	for (i = 0; i < idx; i++)
-	{
-		if (ptr == NULL)
-		{
-			printf("(nil)");
-			return (NULL);
-		}
-
+	for (i = 0; ptr != NULL; i++)
 		ptr = ptr->next;
+	if (idx == 0)
+		temp = add_dnodeint(h, n);
+	else if (idx == i)
+		temp = add_dnodeint_end(h, n);
+	else
+	{
+		for (i = 0; i < idx; i++)
+		{
+			if (ptr == NULL)
+				return (NULL);
+			ptr = ptr->next;
+		}
+		temp->n = n;
+		temp->next = ptr;
+		temp->prev = ptr->prev;
+		ptr->prev = temp;
+		ptr = temp->prev;
+		ptr->next = temp;
 	}
-	temp->next = ptr;
-	temp->prev = ptr->prev;
-	ptr->prev = temp;
-	ptr = temp->prev;
-	ptr->next = temp;
-
 	return (temp);
 }
-
-/**
- * To-Do :  Variables Description
- *          Formt document
- */
