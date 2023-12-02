39596341f37c2dec89a8fc3331574aaef3326b26
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-02 11:47:32 +0000

    8-delete_dnodeint

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -50,7 +50,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		ptrRight = ptr->next;
 		ptrleft->next = ptrRight;
 		ptrRight->prev = ptrleft;
-		free(ptr);
 		return (1);
 	}
 	return (-1);
