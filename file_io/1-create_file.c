d983bca4066d46b2f7a589e867a27a687d6344ca
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-06 14:36:27 +0000

    1-create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,11 +14,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 	if (text_content != NULL)
 		write(fd, text_content, strlen(text_content));
 	return (1);
 }
-
 
 /**
  * To-Do :  Variables Description
