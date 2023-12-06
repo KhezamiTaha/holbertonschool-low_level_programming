5c7fe9b0d6b4ebfef45a3e4b377633b7584cca20
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-06 14:56:34 +0000

    2-append_text_to_file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - Algorithms function
+ *@filename: pointer
+ *@text_content: pointer
+ *
+ *Return: 1 or 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+	return (1);
+}
+
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
