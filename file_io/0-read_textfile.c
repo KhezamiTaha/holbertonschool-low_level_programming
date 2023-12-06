dd0de54f9f498827a5169a8dc08f70a21dbc8de9
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-06 09:26:00 +0000

    0-read_textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *read_textfile - Algorithms function
+ *@filename: pointer
+ *@letters: pointer
+ *
+ *Return: 1 or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int filedescriptor;
+	char buffer[7000];
+
+	filedescriptor = open(filename, O_RDONLY);
+	read(filedescriptor, buffer, letters);
+	write(1, buffer, letters);
+	return (letters);
+}
+
+
+/**
+ * To-Do :  Variables Description
+ *          Formt document
+ */
