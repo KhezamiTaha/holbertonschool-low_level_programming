692668eeecb6794a6e998dced152636c2f0539f9
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-06 09:43:45 +0000

    0-read_textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -14,10 +14,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	filedescriptor = open(filename, O_RDONLY);
 	n = read(filedescriptor, buffer, letters);
+	if (n == -1)
+		return (0);
 	write(1, buffer, n);
 	close(filedescriptor);
-	if (n == -1)
-		n = 0;
 	return (n);
 }
 
