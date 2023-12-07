6a31dfa787ead3074ba451f037104da59bc6b7e0
KhezamiTaha <khezamitaha10@gmail.com>
2023-12-07 09:59:25 +0000

    3-cp

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,32 +16,23 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
 	while (n == 1024)
 	{
 		n = read(fd_from, buffer, n);
-		if (n == -1)
+		if (n == -1 || fd_from == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			exit(98);
 		}
 		n = write(fd_to, buffer, n);
-		if (n == -1)
+		if (n == -1 || fd_to == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
 	}
+
 	if (close(fd_from) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d", fd_from);
